9b579684a07dbfa29635cb889c07261bae57e9b0
ROM <rom.void95@gmail.com>
2024-02-19 12:28:17 -0600

    Update StringUtil.java

--- src/main/java/space/tscg/misc/StringUtil.java
+++ src/main/java/space/tscg/misc/StringUtil.java
@@ -152,7 +152,7 @@ public class StringUtil
     
     private static final char[] DIGITS_LOWER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-    static String encodedHexString(final String string)
+    public static String encodedHexString(final String string)
     {
         return bytesToHex(string.getBytes());
     }
